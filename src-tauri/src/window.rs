df51b395f5efa836cce0aa386e7f552392ac558e
Takagi Tasuku <tasuren@outlook.jp>
2025-05-30 12:56:22 +0900

    Add debug options to window creation

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -17,6 +17,12 @@ pub fn setup_windows(app: &mut tauri::App) {
         .available_monitors()
         .expect("Failed to get available monitors.");
 
+    #[cfg(debug_assertions)]
+    let primary_monitor = app
+        .primary_monitor()
+        .expect("Failed to get primary monitor.")
+        .unwrap();
+
     for (i, monitor) in monitors.into_iter().enumerate() {
         let tauri::LogicalSize { width, height } =
             monitor.size().to_logical::<f64>(monitor.scale_factor());
@@ -25,7 +31,7 @@ pub fn setup_windows(app: &mut tauri::App) {
 
         let window = tauri::WebviewWindowBuilder::new(
             app,
-            format!("draw-window-{i}"),
+            format!("draw-{i}"),
             tauri::WebviewUrl::App("/".into()),
         )
         .title(format!("Monica Draw Window ({i})"))
@@ -38,9 +44,21 @@ pub fn setup_windows(app: &mut tauri::App) {
         .build()
         .expect("Failed to create window");
 
-        window.set_ignore_cursor_events(true).unwrap();
+        if !DISABLE_IGNORE_CURSOR_EVENTS {
+            window.set_ignore_cursor_events(true).unwrap();
+        }
 
         #[cfg(target_os = "macos")]
         setup_macos_window(&window);
+
+        // For debugging
+        if OPEN_ALL_DRAWING_DEVTOOLS
+            || cfg!(debug_assertions) && monitor.name() == primary_monitor.name()
+        {
+            window.open_devtools();
+        }
     }
 }
+
+const OPEN_ALL_DRAWING_DEVTOOLS: bool = option_env!("OPEN_ALL_DRAWING_DEVTOOLS").is_some();
+const DISABLE_IGNORE_CURSOR_EVENTS: bool = option_env!("DISABLE_IGNORE_CURSOR_EVENTS").is_some();

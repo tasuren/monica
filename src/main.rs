bb19b1f2cd4dc11742b1c7fafece4e6010da2efb
Takagi Tasuku <tasuren@outlook.jp>
2025-12-01 21:56:46 +0900

    chore: prevent opening console window on Windows in release

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use gpui::{App, AppContext, WindowId, px, size};
 
 use crate::{

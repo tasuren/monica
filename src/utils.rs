f14374f4680ae0816a1e55a43a5d03adaac5490c
Takagi Tasuku <tasuren_0618@yahoo.co.jp>
2025-12-02 16:14:06 +0900

    chore: add inline macro to coordinates utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,11 @@
 use gpui::{Pixels, Point, Size, point, px, size};
 
+#[inline]
 pub fn dpi_size_to_gpui(logical_size: dpi::LogicalSize<u32>) -> Size<Pixels> {
     size(px(logical_size.width as _), px(logical_size.height as _))
 }
 
+#[inline]
 pub fn dpi_pos_to_gpui(logical_pos: dpi::LogicalPosition<i32>) -> Point<Pixels> {
     point(px(logical_pos.x as _), px(logical_pos.y as _))
 }
